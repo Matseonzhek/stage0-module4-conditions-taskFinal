df15ae2e97f8d3320c646ceabffcaf480ac75876
Tanya Matseonzhek <matseonzhek@gmail.com>
2022-07-30 21:49:26 +0400

    Module4 taskFinal

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class CoordinatePane {
     public void printQuadrant(int x, int y) {
+        int quadrant = x == 0 && y == 0 ? 0 : x > 0 && y > 0 ? 1 : x < 0 && y > 0 ? 2 : x < 0 && y < 0 ? 3 : 4;
+        switch (quadrant) {
+            case (0) -> System.out.println("zero");
+            case (1) -> System.out.println("first");
+            case (2) -> System.out.println("second");
+            case (3) -> System.out.println("third");
+            case (4) -> System.out.println("fourth");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        if (divider != 0) {
+            int result = dividend / divider;
+            System.out.println(divider * result == dividend ? "can be divided completely" : "cannot be divided completely");
+        } else System.out.println("division by zero");
     }
 }
